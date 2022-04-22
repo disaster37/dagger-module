ee9b35529e833bc4e70e55bfd855bd71b2699b64
disaster37 <linuxworkgroup@hotmail.com>
2022-04-22 11:46:28 +0200

    fix: fix install kubeval

--- helm/_scripts/install_kubeval.sh
+++ helm/_scripts/install_kubeval.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 URL=$1
-curl -s ${URL} -o- | tar xvz -C /usr/local/bin --strip-components=0
+curl -o- -L ${URL} | tar xvz -C /usr/local/bin --strip-components=0
 chmod +x /usr/local/bin/kubeval
